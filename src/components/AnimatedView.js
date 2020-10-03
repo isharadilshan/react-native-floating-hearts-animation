6a691a9d3e565ffa57bdeb7d93a7a2619a9e5ba9
Ishara Dilshan <isharadilshandev@gmail.com>
2020-10-03 20:31:33 +0530

    task: added react native vector icons and floating action button

---
+++ src/components/AnimatedView.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const AnimatedView = () => {
+  return (
+    <View>
+      <Text>Animated View</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  animatedWrapper: {
+    flex: 1,
+  },
+});
+
+export default AnimatedView;
---
+++ src/components/FloatingActionButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+
+const FloatingActionButton = ({onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.fab}>
+      <Icon name="add" size={30} style={styles.icon} color={'white'} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 5,
+    width: 56,
+    height: 56,
+    borderRadius: 56 / 2,
+    right: 20,
+    bottom: 30,
+    backgroundColor: '#4a148c',
+  },
+});
+
+export default FloatingActionButton;
